78d7a984e2b6f2814e3e0ec15502411cd31ce79f
Liz Chaddock <lizchaddock@bloodjourney-lm.corp.yahoo.com>
2015-09-16 16:30:50 -0700

    Table view loading

---
+++ Yelp/BusinessCell.h
@@ -0,0 +1,16 @@
+//
+//  BusinessCell.h
+//  Yelp
+//
+//  Created by Liz Chaddock on 9/15/15.
+//  Copyright (c) 2015 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Business.h"
+
+@interface BusinessCell : UITableViewCell
+
+@property (nonatomic, strong) Business * business;
+
+@end
---
+++ Yelp/FiltersViewController.h
@@ -0,0 +1,13 @@
+//
+//  FiltersViewController.h
+//  Yelp
+//
+//  Created by Liz Chaddock on 9/16/15.
+//  Copyright (c) 2015 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FiltersViewController : UIViewController
+
+@end
